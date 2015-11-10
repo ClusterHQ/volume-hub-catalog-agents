78131a9ff4f9de5842c387832561029a165bc68e
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2015-11-10 13:43:20 -0500

    Pretty sure those were bugs, fix them before I try to run this

--- catalog_client/agents/_filelogs.py
+++ catalog_client/agents/_filelogs.py
@@ -8,9 +8,9 @@ class _SyslogCollector(object):
     _LOG_PATHS = {
         # The paths we configure Flocker to log to in the upstart configuration
         # file.  See Flocker/admin/package-files/upstart/flocker-*.conf
-        FilePath(b"/var/log/flocker/flocker-dataset-agent.log"),
-        FilePath(b"/var/log/flocker/flocker-container-agent.log"),
-        FilePath(b"/var/log/flocker/flocker-control.log"),
+        FilePath(b"/host/var/log/flocker/flocker-dataset-agent.log"),
+        FilePath(b"/host/var/log/flocker/flocker-container-agent.log"),
+        FilePath(b"/host/var/log/flocker/flocker-control.log"),
     }
 
     def detect(self):
@@ -20,7 +20,7 @@ class _SyslogCollector(object):
         if self._log_streams is None:
             self._log_streams, self._recorder = self._start_log_streams()
 
-        return self._recorder.consume()
+        return succeed(self._recorder.consume())
 
     def _start_log_streams(self):
         recorder = _MultiStreamRecorder()
@@ -53,9 +53,12 @@ class _FileLogStream(object):
     def _next(self, log_file):
         # Try to read more than one line per iteration but also try to avoid
         # doing an unbounded amount of work.  100 lines is picked out of a hat.
+        lines = []
         for i in range(100):
             line = log_file.readline()
             if line:
-                self.record_log(line)
+                lines.append(line)
             else:
                 break
+        if lines:
+            self.record_log(lines)
