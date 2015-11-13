1fb4874c728fd40a3dd2296efe004b81a32330c5
Luke Marsden <luke@clusterhq.com>
2015-11-12 22:44:59 -0800

    be smarter about which services to run where, support upgrades

--- go.sh
+++ go.sh
@@ -1,21 +1,33 @@
 #!/bin/sh
+if [ "$TARGET" = "control-service" ]; then
+    sudo docker rm -f volume-hub-agent-dataset || false
+    sudo -E docker run -d --restart=always \
+        -e CATALOG_FIREHOSE_SECRET="$TOKEN" \
+        -v /etc/flocker:/etc/flocker \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        --name volume-hub-agent-dataset \
+        clusterhq/catalog-agents-dataset
+elif [ "$TARGET" = "agent-node" ]; then
+    sudo docker rm -f volume-hub-agent-node || false
+    sudo -E docker run -d --restart=always \
+        -e CATALOG_FIREHOSE_SECRET="$TOKEN" \
+        -v /etc/flocker:/etc/flocker \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        --name volume-hub-agent-node \
+        clusterhq/catalog-agents-node
+    sudo docker rm -f volume-hub-agent-docker || false
+    sudo -E docker run -d --restart=always \
+        -e CATALOG_FIREHOSE_SECRET="$TOKEN" \
+        -v /etc/flocker:/etc/flocker \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        --name volume-hub-agent-docker \
+        clusterhq/catalog-agents-docker
+fi
+# This runs on all of them
+sudo docker rm -f volume-hub-agent-log || false
 sudo -E docker run -d --restart=always \
     -e CATALOG_FIREHOSE_SECRET="$TOKEN" \
     -v /etc/flocker:/etc/flocker \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    clusterhq/catalog-agents-docker
-sudo -E docker run -d --restart=always \
-    -e CATALOG_FIREHOSE_SECRET="$TOKEN" \
-    -v /etc/flocker:/etc/flocker \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    clusterhq/catalog-agents-dataset
-sudo -E docker run -d --restart=always \
-    -e CATALOG_FIREHOSE_SECRET="$TOKEN" \
-    -v /etc/flocker:/etc/flocker \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    clusterhq/catalog-agents-node
-sudo -E docker run -d --restart=always \
-    -e CATALOG_FIREHOSE_SECRET="$TOKEN" \
-    -v /etc/flocker:/etc/flocker \
-    -v /var/run/docker.sock:/var/run/docker.sock \
+    --name volume-hub-agent-log \
     clusterhq/catalog-agents-log
