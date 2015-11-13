f4191d579bfd4532e0397babef90d581a32ce00b
Luke Marsden <luke@clusterhq.com>
2015-11-13 14:29:29 +0000

    bind mount / only

--- go.sh
+++ go.sh
@@ -4,34 +4,33 @@ if [ "$TARGET" = "control-service" ]; then
     sudo docker pull clusterhq/catalog-agents-dataset
     sudo -E docker run -d --restart=always \
         -e CATALOG_FIREHOSE_SECRET="$TOKEN" \
-        -v /etc/flocker:/etc/flocker \
-        -v /var/run/docker.sock:/var/run/docker.sock \
+        -v /:/host \
         --name volume-hub-agent-dataset \
         clusterhq/catalog-agents-dataset
+
 elif [ "$TARGET" = "agent-node" ]; then
     sudo docker rm -f volume-hub-agent-docker || false
     sudo docker pull clusterhq/catalog-agents-docker
     sudo -E docker run -d --restart=always \
         -e CATALOG_FIREHOSE_SECRET="$TOKEN" \
-        -v /etc/flocker:/etc/flocker \
-        -v /var/run/docker.sock:/var/run/docker.sock \
+        -v /:/host \
         --name volume-hub-agent-docker \
         clusterhq/catalog-agents-docker
 fi
+
 # This runs on all of them
 sudo docker rm -f volume-hub-agent-log || false
 sudo docker pull clusterhq/catalog-agents-log
 sudo -E docker run -d --restart=always \
     -e CATALOG_FIREHOSE_SECRET="$TOKEN" \
-    -v /etc/flocker:/etc/flocker \
-    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v /:/host \
     --name volume-hub-agent-log \
     clusterhq/catalog-agents-log
+
 sudo docker rm -f volume-hub-agent-node || false
 sudo docker pull clusterhq/catalog-agents-node
 sudo -E docker run -d --restart=always \
     -e CATALOG_FIREHOSE_SECRET="$TOKEN" \
-    -v /etc/flocker:/etc/flocker \
-    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v /:/host \
     --name volume-hub-agent-node \
     clusterhq/catalog-agents-node
