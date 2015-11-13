87d8c775384ff3a38531247edc4af9e1bcf78cbe
Luke Marsden <luke@clusterhq.com>
2015-11-12 22:39:15 -0800

    Add node dockerfile, make go.sh run more

--- go.sh
+++ go.sh
@@ -4,3 +4,18 @@ sudo -E docker run -d --restart=always \
     -v /etc/flocker:/etc/flocker \
     -v /var/run/docker.sock:/var/run/docker.sock \
     clusterhq/catalog-agents-docker
+sudo -E docker run -d --restart=always \
+    -e CATALOG_FIREHOSE_SECRET="$TOKEN" \
+    -v /etc/flocker:/etc/flocker \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    clusterhq/catalog-agents-dataset
+sudo -E docker run -d --restart=always \
+    -e CATALOG_FIREHOSE_SECRET="$TOKEN" \
+    -v /etc/flocker:/etc/flocker \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    clusterhq/catalog-agents-node
+sudo -E docker run -d --restart=always \
+    -e CATALOG_FIREHOSE_SECRET="$TOKEN" \
+    -v /etc/flocker:/etc/flocker \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    clusterhq/catalog-agents-log
